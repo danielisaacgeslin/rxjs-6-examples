b6b4daffb3509f4e3e3b54436282e368b45426f6
Daniel Isaac Geslin <isaacgeslin@gmail.com>
2018-11-12 13:00:12 -0300

    custom operator example

--- example-1.ts
+++ example-1.ts
@@ -1,5 +1,27 @@
 // https://thinkster.io/tutorials/learn-rxjs-observables/subjects-observables-and-operators
 
+// Observer
+// You usually won't interact with the Observer object directly, as you'll likely interact with a Subject instead (which we cover below),
+// but it's important to know what it does.
+// Observers allow you to "push" new data into an observable sequence. You can think of this as a "Write-only" way of modifying
+// observable sequences (to go back to our analogy of assembly lines, observers can only add new cars onto an assembly line).
+
+// Observable
+// An Observable is what we can use to listen, aka subscribe, to new changes that are emitted by an Observer.
+// Think of this as a "Read-only" assembly line (you can only observe when new cars come off the assembly line).
+
+// Subject
+// A Subject is simply an Observer and Observable. You can push new values as well as subscribe to it.
+// Think of this as a "Read & Write" assembly line (you can both add cars onto the assembly line and observe cars that come off the assembly line).
+
+// Operators
+// The purpose of Operators in RxJS are the same as most operators in other programming
+// languages/libraries: they allow you to perform operations on your code.
+
+// In RxJS, you can think of Operators as a way to manipulate the data coming from a Subject (or Observer) before it's sent to an Observable.
+// This is the equivalent of instructing an assembly line to modify the car in a certain way (i.e. paint it black, shine it, etc) and
+// then return it to the next assembly line.
+
 /** observable sin rxjs */
 
 const observable = {
@@ -10,7 +32,7 @@ const observable = {
     setTimeout(() => error(new Error('some error')), 2000);
     setTimeout(() => next(4), 3000);
     setTimeout(() => complete(), 4000);
-    
+
   }
 }
 
--- example-4.ts
+++ example-4.ts
@@ -1,5 +1,5 @@
 /** of (observable) con operadores */
-import { of } from 'rxjs';
+import { of, Observable } from 'rxjs';
 import { map, filter, distinct } from 'rxjs/operators';
 
 console.log('\n FIRST map');
@@ -34,3 +34,24 @@ of(1, 2, 3, 4, 1, 2, 1, 3, 4, 1, 5, 2, 6, 3, 4, 5, 7, 1, 1, 1, 1, 1).pipe(
 // value is: 15
 // value is: 16
 // value is: 17
+
+console.log('\n FOURTH custom operator');
+/** add returns a function that returns a function that returns an observable */
+const add = (numberToAdd: number) => (source: Observable<number>) => (
+  /** creating a new observable with a custom subscribe function */
+  new Observable(subscriber => {
+    const subscription = source.subscribe({
+      next: value => subscriber.next(value + numberToAdd),
+      error: e => subscriber.error(e),
+      complete: () => subscriber.complete()
+    });
+    return subscription;
+  })
+);
+of(1, 2, 3).pipe(
+  add(10), // 1 + 10...then 2 + 10...then 3 + 10
+  add(20) // 11 + 20...then 12 + 20...then 13 + 20
+).subscribe(value => console.log(`value is: ${value}`));
+// value is: 31
+// value is: 32
+// value is: 33
